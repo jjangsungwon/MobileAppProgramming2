2d97d2f95c0de22e08d6187680324b1a6006e3bf
chanhuiseok <chanhuicom@gmail.com>
2020-06-03 16:47:20 +0900

    re_init

---
+++ fokin-weather/README.md
@@ -0,0 +1,2 @@
+# fokin-weather
+Learning React Native by Building a Fokin-weahter App
---
+++ fokin-weather/Weather.js
@@ -0,0 +1,162 @@
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const weatherOptions = {
+    Thunderstorm: {
+      iconName: "weather-lightning",
+      gradient: ["#373B44", "#4286f4"],
+      title: "Thunderstorm in the house",
+      subtitle: "Actually, outside of the house"
+    },
+    Drizzle: {
+      iconName: "weather-hail",
+      gradient: ["#89F7FE", "#66A6FF"],
+      title: "Drizzle",
+      subtitle: "Is like rain, but gay 🏳️‍🌈"
+    },
+    Rain: {
+      iconName: "weather-rainy",
+      gradient: ["#00C6FB", "#005BEA"],
+      title: "Raining like a MF",
+      subtitle: "For more info look outside"
+    },
+    Snow: {
+      iconName: "weather-snowy",
+      gradient: ["#7DE2FC", "#B9B6E5"],
+      title: "Cold as balls",
+      subtitle: "Do you want to build a snowman? Fuck no."
+    },
+    Atmosphere: {
+      iconName: "weather-hail",
+      gradient: ["#89F7FE", "#66A6FF"]
+    },
+    Clear: {
+      iconName: "weather-sunny",
+      gradient: ["#FF7300", "#FEF253"],
+      title: "Sunny as fuck",
+      subtitle: "맑음"
+    },
+    Clouds: {
+      iconName: "weather-cloudy",
+      gradient: ["#D7D2CC", "#304352"],
+      title: "구름 많음",
+      subtitle: "흐림"
+    },
+    Mist: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Mist!",
+      subtitle: "It's like you have no glasses on."
+    },
+    Dust: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Dusty",
+      subtitle: "Thanks a lot China 🖕🏻"
+    },
+    Haze: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Haze",
+      subtitle: "Just don't go outside."
+    }
+  };
+  
+  export default function Weather({ name, temp, condition }) {
+    return (    
+      <LinearGradient
+        colors={weatherOptions[condition].gradient}
+        style={styles.container}
+      >
+        <StatusBar barStyle="light-content" />
+        <View style={styles.halfContainer}>
+          <MaterialCommunityIcons
+            size={96}
+            name={weatherOptions[condition].iconName}
+            color="white"
+          />
+          <Text style={styles.temp}>{temp}°</Text>
+        </View>
+        <View style={styles.textContainer}>
+            
+          <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+          <Text style={styles.subtitle}>
+            {weatherOptions[condition].subtitle}
+          </Text>
+          
+          <Text style={styles.temp}>{name}</Text>
+        </View>
+      </LinearGradient>
+    );
+  }
+
+  Weather.PropTypes={
+      temp:PropTypes.number.isRequired,
+      condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Mist",
+        "Dust"
+      ]).isRequired
+  };
+
+  Weather.propTypes = {
+    temp: PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+      "Thunderstorm",
+      "Drizzle",
+      "Rain",
+      "Snow",
+      "Atmosphere",
+      "Clear",
+      "Clouds",
+      "Haze",
+      "Mist",
+      "Dust"
+    ]).isRequired
+  };
+  
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+    temp: {
+      fontSize: 42,
+      color: "white"
+    },
+    halfContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    title: {
+      color: "white",
+      fontSize: 44,
+      fontWeight: "300",
+      marginBottom: 10,
+      textAlign: "left"
+    },
+    subtitle: {
+      fontWeight: "600",
+      color: "white",
+      fontSize: 24,
+      textAlign: "left"
+    },
+    textContainer: {
+      alignItems: "flex-start",
+      paddingHorizontal: 40,
+      justifyContent: "center",
+      flex: 1
+    }
+  });
+
