625026eca3d0422bc2f226263ae790c65e906cec
ChanhuiSeok <chanhuicom@gmail.com>
2020-06-12 22:22:33 +0900

    swift init

---
+++ weatherProject_swift/weatherProject_swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherProject_swift
+//
+//  Created by chan on 2020/06/12.
+//  Copyright © 2020 COMP420. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
